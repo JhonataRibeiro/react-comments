e1ab29f827f526328f4b4f5d7576b882cdd81983
Jhonata Ribeiro <jhonata.ribeiro.ti@gmail.com>
2016-07-04 13:05:32 -0300

    create simple comments

--- src/app/components/Comments/Comments.js
+++ src/app/components/Comments/Comments.js
@@ -1,3 +1,17 @@
 /**
  * Created by jhonataalmeida on 7/4/16.
  */
+import React, { Component, PropType} from "react";
+
+class Comments extends Component{
+	render(){
+		return(
+			<div>
+				<h4>{this.props.author}</h4>
+				<h4>{this.props.text}</h4>
+			</div>)
+	}
+}
+
+export default Comments;
+
