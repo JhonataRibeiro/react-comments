3daf20b22d03963b5b8e46db5a4a55181ec97e7d
Jhonata Ribeiro <jhonata.ribeiro.ti@gmail.com>
2016-07-04 13:05:55 -0300

    create comments list

--- src/app/components/CommentList/CommentList.js
+++ src/app/components/CommentList/CommentList.js
@@ -1,3 +1,26 @@
 /**
  * Created by jhonataalmeida on 7/4/16.
  */
+import React,{ Component, PropType } from 'react';
+import Comments from '../Comments/Comments';
+
+class CommentList extends Component{
+	render(){
+
+		var commentNodes = this.props.data.map((comment) => {
+			return(
+				<Comments author={comment.author} key={comment.id} text={comment.text}>
+					lala
+				</Comments>
+			);
+		})
+
+		return(
+			<div>
+				{ commentNodes }
+			</div>
+		)
+	}
+}
+
+export default CommentList;
