68da27b96b51bde56fe89ae6b159c5e61f8b683e
Jhonata Ribeiro <jhonata.ribeiro.ti@gmail.com>
2016-07-12 17:01:34 -0300

    add remarkable

--- src/app/components/Comments/Comments.js
+++ src/app/components/Comments/Comments.js
@@ -2,13 +2,26 @@
  * Created by jhonataalmeida on 7/4/16.
  */
 import React, { Component, PropType} from "react";
+import Remarkable from "remarkable";
 
 class Comments extends Component{
+
+	constructor(){
+		super();
+		this.rawMarkup = this.rawMarkup.bind(this);
+	}
+
+	rawMarkup(){
+		var md = new Remarkable();
+		var markup = md.render(this.props.children.toString());
+		return{ __html:markup };
+	}
+
 	render(){
 		return(
 			<div>
 				<h4>{this.props.author}</h4>
-				<h4>{this.props.text}</h4>
+				<span dangerouslySetInnerHTML={this.rawMarkup()} />
 			</div>)
 	}
 }
