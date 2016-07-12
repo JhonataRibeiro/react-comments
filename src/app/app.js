9349b7fac81789de8ffc88ab9e03470b437382f8
Jhonata Ribeiro <jhonata.ribeiro.ti@gmail.com>
2016-07-11 23:39:29 -0300

    realocate const data

--- src/app/app.js
+++ src/app/app.js
@@ -2,14 +2,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import CommentBox from './container/CommentBox'; // Our custom react component
 
-// Data to start.
-
-const data = [
-	{id: 1, author: "Jhonata Ribeiro", text: "This is one comment"},
-	{id: 2, author: "Jhon Stuart", text: "This is *another* comment"}
-];
-
 // Render the main app react component into the app div.
 // For more details see: https://facebook.github.io/react/docs/top-level-api.html#react.render
 
-render(<CommentBox data = { data } />, document.getElementById('app'));
+render(<CommentBox/>, document.getElementById('app'));
